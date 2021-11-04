b1c8036a65d5ce5ae772b9a399644985d5dc3e0c
yudi <yudikahn8@gmail.com>
2021-11-04 23:30:34 +0200

    msg

--- server.js
+++ server.js
@@ -6,12 +6,12 @@ const connectDb = require('./server/service/connectDb');
 require('dotenv').config();
 
 //force https
-app.use((req,res,next)=>{
-    if(!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV === 'production'){
-        return res.redirect('https://' + req.get('host') + req.url);
-    }
-    return next();
-});
+// app.use((req,res,next)=>{
+//     if(!req.secure && req.get('x-forwarded-proto') !== 'https' && process.env.NODE_ENV === 'production'){
+//         return res.redirect('https://' + req.get('host') + req.url);
+//     }
+//     return next();
+// });
 
 app.use(express.json({strict: false}));
 app.use(express.static('server/public'));
@@ -33,7 +33,6 @@ useApi(app);
         res.sendFile(path.resolve(__dirname, 'client', 'build', 'index.html'))
     })
 //}
-
 
 const listener = app.listen(process.env.PORT||8080, ()=>{
     if(process.env.NODE_ENV !== 'production'){
